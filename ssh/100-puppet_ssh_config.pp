294d0d371cfb1426191f3ddd1f852a913c146a07
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-08-03 09:42:38 +0200

    SSH

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#don't use password
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
