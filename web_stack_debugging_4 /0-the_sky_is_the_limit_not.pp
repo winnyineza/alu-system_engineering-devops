3eaeb862b728a84150e8122f1ccedd8cffea52e6
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-08-28 15:46:28 +0200

    Updated

---
+++ web_stack_debugging_4 /0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# allows  simulation of  HTTP requests to a web server
+
+exec { 'replace_limit':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx; service nginx restart',
+}
